ed81a6a960e98e093efc4370a77bfca8e9acd7f9
Hugo Ignacio <hugo.ignacio1998@gmail.com>
2024-02-09 19:36:02 -0300

    Add CustomerController with create and get methods

---
+++ src/main/java/com/boardcamp/api/controllers/CustomerController.java
@@ -0,0 +1,39 @@
+package com.boardcamp.api.controllers;
+
+import com.boardcamp.api.dtos.CustomerDTO;
+import com.boardcamp.api.models.CustomerModel;
+import com.boardcamp.api.services.CustomerService;
+
+import jakarta.validation.Valid;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/customers")
+public class CustomerController {
+    final CustomerService customerService;
+
+    CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @PostMapping
+    public ResponseEntity<CustomerModel> createCustomer(@RequestBody @Valid CustomerDTO body) {
+        CustomerModel customer = customerService.save(body);
+        return ResponseEntity.status(HttpStatus.CREATED).body(customer);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CustomerModel> getCustomerById(@PathVariable Long id) {
+        CustomerModel customer = customerService.findById(id);
+        return ResponseEntity.status(HttpStatus.OK).body(customer);
+    }
+
+}
