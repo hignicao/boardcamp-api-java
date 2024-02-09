d7bedadda2167935dac4d8c27dcd114e7395d326
Hugo Ignacio <hugo.ignacio1998@gmail.com>
2024-02-09 19:36:36 -0300

    Add RentalController class

---
+++ src/main/java/com/boardcamp/api/controllers/RentalController.java
@@ -0,0 +1,48 @@
+package com.boardcamp.api.controllers;
+
+import com.boardcamp.api.dtos.RentalDTO;
+import com.boardcamp.api.models.RentalModel;
+import com.boardcamp.api.services.RentalService;
+
+import jakarta.validation.Valid;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rentals")
+public class RentalController {
+    private final RentalService rentalService;
+
+    public RentalController(RentalService rentalService) {
+        this.rentalService = rentalService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<RentalModel>> getRentals() {
+        List<RentalModel> rentals = rentalService.findAll();
+        return ResponseEntity.status(HttpStatus.OK).body(rentals);
+    }
+
+    @PostMapping
+    public ResponseEntity<RentalModel> createRental(@RequestBody @Valid RentalDTO body) {
+        RentalModel rental = rentalService.save(body);
+        return ResponseEntity.status(HttpStatus.CREATED).body(rental);
+    }
+
+    @PutMapping("/{id}/return")
+    public ResponseEntity<RentalModel> returnRental(@PathVariable Long id) {
+        RentalModel rental = rentalService.update(id);
+        return ResponseEntity.status(HttpStatus.OK).body(rental);
+    }
+
+}
